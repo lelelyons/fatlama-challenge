4f308dfa907b8caa21a36c162cdd6714d69cd06d
YOURNAME <YOUREMAIL>
2018-08-19 14:19:22 +0100

    filter queryset by distances from given lat and lng

--- search/models.py
+++ search/models.py
@@ -1,4 +1,46 @@
 from django.db import models
+from scipy.spatial import distance
+import heapq
+import pdb
+
+def filter_items(query, k):
+    queryset = Items.objects.all()
+
+    search_term, lat, lng = get_query_params(query)
+
+    if search_term is not None:
+        queryset = queryset.filter(item_name__contains=search_term)
+    else:
+        return []
+
+    if lat is not None and lng is not None:
+        distances = calculate_distances(lat, lng, queryset)
+        k_closest_items = closest_items(distances, k)
+    else:
+        return queryset[:20]
+
+    return list(map(lambda i: i['queryset'], k_closest_items))
+
+def get_query_params(query):
+    search_term = query.get('searchTerm', None)
+    lat = query.get('lat', None)
+    lng = query.get('lng', None)
+    return search_term, lat, lng
+
+def calculate_distances(lat, lng, queryset):
+    distances = [None] * len(queryset)
+
+    for i, item in enumerate(queryset):
+        distances[i] = { 'distance': distance.pdist(
+            [[float(lat), float(lng)], [item.get_lat(), item.get_lng()]],
+            'euclidean'
+        )[0],
+            'queryset': item
+            }
+    return distances
+
+def closest_items(points, k):
+    return heapq.nsmallest(k, points, key=lambda p : p['distance'])
 
 class Items(models.Model):
     item_name = models.CharField(max_length=255, null=False, primary_key=True)
@@ -7,3 +49,11 @@ class Items(models.Model):
     item_url = models.CharField(max_length=255, null=False)
     img_urls = models.CharField(max_length=255, null=False)
 
+    def get_lat(self):
+        return self.lat
+
+    def get_lng(self):
+        return self.lng
+
+    def get_name(self):
+        return self.item_name
--- search/views.py
+++ search/views.py
@@ -1,20 +1,14 @@
 from rest_framework import generics
-from .models import Items
+from .models import Items, filter_items
 from .serializers import ItemsSerializer
 
-import pdb
+NO_ITEMS = 3
+
 class ListItemsView(generics.ListAPIView):
     serializer_class = ItemsSerializer
 
     def get_queryset(self):
-            queryset = Items.objects.all()
-            return self.filterItems(queryset)
+        return filter_items(self.request.query_params, NO_ITEMS)
 
-    def filterItems(self, queryset):
-        search_term = self.request.query_params.get('searchTerm', None)
-        lat = self.request.query_params.get('lat', None)
-        lng = self.request.query_params.get('lng', None)
 
-        if search_term is not None:
-            queryset = queryset.filter(item_name__contains=search_term)
-        return queryset
+
